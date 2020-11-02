133725130f7db291c31dac82a9fddae3fa0907b8
acrunyon <amber.c.runyon@gmail.com>
2020-11-02 08:45:57 -0700

    filter section

--- Tidy1.R
+++ Tidy1.R
@@ -13,3 +13,24 @@ this_is_a_really_long_name
 # CH 3 - Data transformations
 nycflights13::flights
 View(flights)
+
+# filter() - pick observatios by their values
+filter(flights, month == 1, day ==1)
+jan1 <- filter(flights, month == 1, day == 1)
+
+# must ve EXACT for == to work, can use near()
+near(sqrt(2)^2,2)
+
+filter(flights, month == 11 | month == 12)
+filter(flights, month %in% c(11, 12))
+
+# arrange()- reorder the rows
+
+
+# select() - pick variables by their names
+
+
+# mutate() - create new var with funcs of existing var
+
+
+# summarise() - collapse many values down to single value
